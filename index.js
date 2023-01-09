f6b8ecb108b002d1ea084019f9b42c88573ef3b4
Sam Chappel <m.samchappel@gmail.com>
2023-01-08 20:15:19 -0800

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 22;
+const num4 = 18;
+const mod = (num3 % num4);
+
+const max = Math.max(1, 3, 2, 20);
